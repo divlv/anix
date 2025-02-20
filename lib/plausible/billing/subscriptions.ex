e3d7e20858b304714f3cc502c7acaa6b7e7c8667
dmitrijszaharovs <dmitrijs.zaharovs@emergn.com>
2025-02-20 13:18:55 +0200

    Subscription updated

--- lib/plausible/billing/subscriptions.ex
+++ lib/plausible/billing/subscriptions.ex
@@ -4,17 +4,17 @@ defmodule Plausible.Billing.Subscriptions do
   require Plausible.Billing.Subscription.Status
   alias Plausible.Billing.Subscription
 
-  def active?(_), do: true
+  def active?(subscription), do: true
 
   @spec expired?(Subscription.t()) :: boolean()
-  def expired?(_), do: false
+  def expired?(subscription), do: false
 
-  def resumable?(_), do: true
+  def resumable?(subscription), do: true
 
-  def halted?(_), do: false
+  def halted?(subscription), do: false
 
   # Helper to create unlimited enterprise subscription
-  defp create_unlimited_subscription do
+  def create_unlimited_subscription do
     future_date = Date.add(Date.utc_today(), 36500) # 100 years in the future
 
     %Subscription{
@@ -25,11 +25,13 @@ defmodule Plausible.Billing.Subscriptions do
       last_bill_date: Date.utc_today(),
       next_bill_amount: "0",
       currency_code: "USD",
-      update_url: "",
-      cancel_url: ""
+      update_url: "https://checkout.paddle.com/subscription/update",
+      cancel_url: "https://checkout.paddle.com/subscription/cancel",
+      team_id: 1
     }
   end
 
-  # Override any subscription request to return unlimited
+  # Make create_unlimited_subscription public and always return it
+  def get_subscription(nil), do: create_unlimited_subscription()
   def get_subscription(_), do: create_unlimited_subscription()
 end
