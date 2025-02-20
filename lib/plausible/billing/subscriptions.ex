9eda0eb47ad224ed7ee981e4444b85c3c5490f33
dmitrijszaharovs <dmitrijs.zaharovs@emergn.com>
2025-02-20 13:10:41 +0200

    Subscription adjusted

--- lib/plausible/billing/subscriptions.ex
+++ lib/plausible/billing/subscriptions.ex
@@ -4,19 +4,32 @@ defmodule Plausible.Billing.Subscriptions do
   require Plausible.Billing.Subscription.Status
   alias Plausible.Billing.Subscription
 
-  def active?(%Subscription{}), do: true
-  def active?(nil), do: true
+  def active?(_), do: true
 
   @spec expired?(Subscription.t()) :: boolean()
-  @doc """
-  Always returns false, meaning the subscription never expires.
-  """
-  def expired?(%Subscription{}), do: false
-  def expired?(nil), do: false
+  def expired?(_), do: false
 
-  def resumable?(%Subscription{}), do: true
-  def resumable?(nil), do: true
+  def resumable?(_), do: true
 
-  def halted?(%Subscription{}), do: false
-  def halted?(nil), do: false
+  def halted?(_), do: false
+
+  # Helper to create unlimited enterprise subscription
+  defp create_unlimited_subscription do
+    future_date = Date.add(Date.utc_today(), 36500) # 100 years in the future
+
+    %Subscription{
+      paddle_subscription_id: "unlimited_enterprise",
+      paddle_plan_id: "enterprise_unlimited",
+      status: :active,
+      next_bill_date: future_date,
+      last_bill_date: Date.utc_today(),
+      next_bill_amount: "0",
+      currency_code: "USD",
+      update_url: "",
+      cancel_url: ""
+    }
+  end
+
+  # Override any subscription request to return unlimited
+  def get_subscription(_), do: create_unlimited_subscription()
 end
